b6731847199ea4c1750ff774e665537d6522621e
Jon Davis <jon@jondavis.net>
2015-11-24 18:58:36 -0700

    sequential order of setup

--- examples/BrokenRecordConsole/Program.cs
+++ examples/BrokenRecordConsole/Program.cs
@@ -8,16 +8,17 @@ namespace BrokenRecordConsole
         // ReSharper disable once UnusedParameter.Local
         static void Main(string[] args)
         {
-            using (var dispatcher = new ThreadedMessageEventDispatcher<BrokenRecordRequest>("Marco"))
+            var messageSystem = 
+            //new BasicMessageQueueContainer<BrokenRecordRequest>("Marco");
+            new LocalMessagePublishContainer<BrokenRecordRequest>("Marco");
+            using (var requester = new BrokenRecordRequester(messageSystem))
             {
-                var messageSystem = 
-                    //new BasicMessageQueueContainer<BrokenRecordRequest>("Marco");
-                    new LocalMessagePublishContainer<BrokenRecordRequest>("Marco");
-                using (var requester = new BrokenRecordRequester(messageSystem))
+                using (var dispatcher = new ThreadedMessageEventDispatcher<BrokenRecordRequest>("Marco"))
                 {
                     dispatcher.RegisterMessageSource(messageSystem);
                     var responder = new BrokenRecordResponder(dispatcher);
                     dispatcher.Start();
+
                     requester.Start();
 
                     Console.WriteLine("Press ENTER to exit ...");
